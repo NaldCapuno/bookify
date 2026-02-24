26aa1b01dc0c6c1daee5ecccf9ae87168edd5246
NaldCapuno <202280125@psu.palawan.edu.ph>
2026-02-24 09:35:43 +0800

    merge

--- lib/core/database/app_database.dart
+++ lib/core/database/app_database.dart
@@ -1,11 +1,11 @@
+import 'package:drift/drift.dart';
+import 'package:drift_flutter/drift_flutter.dart';
 import 'package:bookkeeping/core/database/daos/users_dao.dart';
 import 'package:bookkeeping/core/database/daos/journal_entry_daos.dart';
 import 'package:bookkeeping/core/database/db_migration.dart';
 import 'package:bookkeeping/core/database/tables/transactions_table.dart';
 import 'package:bookkeeping/core/database/tables/journal_table.dart';
 import 'package:bookkeeping/core/database/tables/user_table.dart';
-import 'package:drift/drift.dart';
-import 'package:drift_flutter/drift_flutter.dart';
 import 'package:bookkeeping/core/database/daos/reports_dao.dart';
 
 // TODO: Import the tables here
@@ -17,13 +17,12 @@ part 'app_database.g.dart';
 @DriftDatabase(
   // TODO: Register the tables and DAOs here
   tables: [AccountCategories, Accounts, Journals, Transactions, Users],
-  daos: [UsersDao, JournalEntryDao],
+  daos: [UsersDao, JournalEntryDao, ReportsDao],
 )
 class AppDatabase extends _$AppDatabase {
   AppDatabase() : super(driftDatabase(name: 'app_db'));
 
   @override
-  int get schemaVersion => 7; // TODO: Don't forget to increase this. (int only)
   int get schemaVersion => 7; // TODO: Don't forget to increase this. (int only)
 
   @override
