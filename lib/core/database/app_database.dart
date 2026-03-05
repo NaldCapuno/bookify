9fc3f9ab39e0d8239fcd6ba4152d3ed6a8380140
kenricklim <kenerol.lim365@gmail.com>
2026-03-05 16:58:52 +0800

    UPDATE

--- lib/core/database/app_database.dart
+++ lib/core/database/app_database.dart
@@ -29,7 +29,7 @@ class AppDatabase extends _$AppDatabase {
   AppDatabase() : super(driftDatabase(name: 'app_db'));
 
   @override
-  int get schemaVersion => 20; // TODO: Don't forget to increase this. (int only)
+  int get schemaVersion => 2; // TODO: Don't forget to increase this. (int only)
 
   @override
   MigrationStrategy get migration => buildMigrationStrategy(this);
